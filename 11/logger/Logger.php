1e715fe2ab66c0cddf59c86c38e6a1d57cf4c712
DinoBambino69 <48675489+DinoBambino69@users.noreply.github.com>
2020-05-14 19:03:44 +0300

    HomeWork_11 add

--- 11/logger/Logger.php
+++ 11/logger/Logger.php
@@ -4,48 +4,10 @@ namespace logger;
 
 use DateTime;
 use DateTimeZone;
+use Psr\Log\LogLevel;
 
 class Logger implements LoggerInterface
 {
-    public function emergency($message, array $context = [])
-    {
-        // TODO: Implement emergency() method.
-    }
-
-    public function alert($message, array $context = [])
-    {
-        // TODO: Implement alert() method.
-    }
-
-    public function critical($message, array $context = [])
-    {
-        // TODO: Implement critical() method.
-    }
-
-    public function error($message, array $context = [])
-    {
-        // TODO: Implement error() method.
-    }
-
-    public function warning($message, array $context = [])
-    {
-        // TODO: Implement warning() method.
-    }
-
-    public function notice($message, array $context = [])
-    {
-        // TODO: Implement notice() method.
-    }
-
-    public function info($message, array $context = [])
-    {
-        // TODO: Implement info() method.
-    }
-
-    public function debug($message, array $context = [])
-    {
-        // TODO: Implement debug() method.
-    }
 
     public function log($level, $message, array $context = [])
     {
@@ -61,5 +23,45 @@ class Logger implements LoggerInterface
         $fp = fopen("file.txt", "w");
         fwrite($fp, $json_code);
         fclose($fp);
+    }
+
+    public function emergency($message, array $context = [])
+    {
+        $this->log(LogLevel::EMERGENCY, $message);
+    }
+
+    public function alert($message, array $context = [])
+    {
+        $this->log(LogLevel::ALERT, $message);
+    }
+
+    public function critical($message, array $context = [])
+    {
+        $this->log(LogLevel::CRITICAL, $message);
+    }
+
+    public function error($message, array $context = [])
+    {
+        $this->log(LogLevel::ERROR, $message);
+    }
+
+    public function warning($message, array $context = [])
+    {
+        $this->log(LogLevel::WARNING, $message);
+    }
+
+    public function notice($message, array $context = [])
+    {
+        $this->log(LogLevel::NOTICE, $message);
+    }
+
+    public function info($message, array $context = [])
+    {
+        $this->log(LogLevel::INFO, $message);
+    }
+
+    public function debug($message, array $context = [])
+    {
+        $this->log(LogLevel::DEBUG, $message);
     }
 }
